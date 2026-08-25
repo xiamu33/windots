fe1e06874d14c9c24554372f961b730fc9c161fa
xiamu <myjyixi@163.com>
2026-08-25 17:11:34 +0800

    feat(pwsh): ✨ add abbr

---
+++ dotfiles/powershell/aliases/common.ps1
@@ -0,0 +1,10 @@
+Set-Abbr ca cargo
+Set-Abbr rsc rustc
+Set-Abbr rsup rustup
+
+Set-Abbr czi chezmoi
+Set-Abbr czia "chezmoi edit --apply"
+Set-Abbr czid "chezmoi diff"
+
+Set-Abbr lzg lazygit
+Set-Abbr zlj zellij
